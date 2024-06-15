64136ecc7bcfcefb5020b153dc04567a18b353f8
adrxan <52432020+adrxan@users.noreply.github.com>
2024-06-15 02:56:28 -0500

    refactor drawing method

--- script.js
+++ script.js
@@ -9,16 +9,12 @@ const inactiveColor = getComputedStyle(document.body).getPropertyValue(
 );
 
 const gridToggle = document.querySelector("#grid-toggle");
+const fullPage = document.querySelector("body");
 let resolution = 16;
 let gridVisible = true;
 
-function toggleGrid() {
-  gridVisible = gridVisible ? false : true;
-  gridToggle.style.color = gridVisible ? accentColor : inactiveColor;
+let isDrawing = false;
 
-  clearPixels();
-  makeGridPixels();
-}
 
 const gridContainer = document.querySelector("#grid-container");
 gridContainer.style.width = gridContainer.style.height = `${gridSize}px`;
@@ -27,14 +23,22 @@ const sliderValue = document.querySelector("#slider-value");
 
 sliderValue.textContent = `${slider.value}²`;
 
-function fillPixel() {
-  this.style.backgroundColor = "black";
+function fillPixel(e) {
+  if (e.type === "mousedown") {
+    isDrawing = true;
+    e.target.style.backgroundColor = "black";
+  }
+  else if (e.type === "mouseover" && isDrawing) {
+    e.target.style.backgroundColor = "black";
+  } 
+  else isDrawing = false;
 }
 
 function makeGridPixels() {
   const numPixels = (resolution * resolution);
+  let widthOrHeight = 0;
 
-  for (let i = 0; i < (resolution * resolution); i++) {
+  for (let i = 0; i < numPixels; i++) {
     const gridPixel = document.createElement("div");
 
     if (gridVisible) {
@@ -47,26 +51,42 @@ function makeGridPixels() {
 
     gridPixel.style.width = gridPixel.style.height = widthOrHeight;
     
-    gridPixel.addEventListener("mouseover", fillPixel);
+    gridPixel.addEventListener("mousedown", (e) => fillPixel(e));
+    gridPixel.addEventListener("mouseover", (e) => fillPixel(e));
+    fullPage.addEventListener("mouseup", (e) => fillPixel(e));
+    gridPixel.addEventListener ("dragstart", (e) => {e.preventDefault()});
+
     gridContainer.appendChild(gridPixel);
   }
 }
 
 makeGridPixels();
 
-function clearPixels() {
+function clearGridPixels() {
   while (gridContainer.firstChild) {
     gridContainer.removeChild(gridContainer.firstChild);
   }
 }
 
 slider.oninput = function () {
-  let resValue = `${this.value}`;
-  sliderValue.textContent = `${resValue}²`;
-  clearPixels();
   resolution = this.value;
+  sliderValue.textContent = `${this.value}²`;
+  clearGridPixels();
   makeGridPixels();
 };
+
+const gridIcon = document.querySelector("#grid-icon");
+
+function toggleGrid() {
+  gridVisible = gridVisible ? false : true;
+  gridIcon.style.color = gridVisible ? accentColor : inactiveColor;
+
+  let r = confirm("This will clear the drawing. Continue?");
+  if (r) {
+    clearGridPixels();
+    makeGridPixels();
+  }
+}
 
 gridToggle.addEventListener("click", toggleGrid);
 
@@ -75,7 +95,7 @@ const clearBtn = document.querySelector("#clear-button");
 clearBtn.addEventListener("click", () => {
   let r = confirm("This will clear the drawing. Continue?");
   if (r) {
-    clearPixels();
+    clearGridPixels();
     makeGridPixels();
   }
 });
